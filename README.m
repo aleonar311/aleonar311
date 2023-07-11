095bab0e3667df955e8a26020a55edbf9858076c
aleonar311 <139006063+aleonar311@users.noreply.github.com>
2023-07-10 18:12:09 -0700

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @aleonar311
+- 👀 I’m interested in ...  2000s Rock and Rap Music And Video Games
+- 🌱 I’m currently learning ... Software Engineering 
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ... aleonar311@gmail.com
+
+<!---
+aleonar311/aleonar311 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
